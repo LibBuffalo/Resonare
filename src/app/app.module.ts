0375d68e4b5f08af60c6051b5cf4c870dcca9695
joosep <joosep.loidap@gmail.com>
2022-08-09 14:13:26 +0300

    Code fix: made it prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MatDividerModule} from '@angular/material/divider';
 import { ProjectsListComponent } from './projects-list/projects-list.component';
@@ -19,13 +19,13 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { AddProjectComponent } from './add-project/add-project.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule} from '@angular/material/datepicker';
-import { MatNativeDateModule, } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
