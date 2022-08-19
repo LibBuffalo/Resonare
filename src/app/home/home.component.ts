f79d596c3383b574e647839226b4f0f762465bf7
joosep <joosep.loidap@gmail.com>
2022-08-19 22:12:58 +0300

    Background colors changed and logo added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface Tile {
-  cols: number;
-  rows: number;
-  text: string;
-}
+// export interface Tile {
+//   cols: number;
+//   rows: number;
+//   text: string;
+// }
 
 @Component({
   selector: 'app-home',
@@ -13,12 +13,12 @@ export interface Tile {
 })
 export class HomeComponent implements OnInit {
 
-  tiles: Tile[] = [
-    {text: 'Resonare', cols: 3, rows: 1},
-    {text: 'Concert Arrangement', cols: 1, rows: 2},
-    {text: 'Project Management', cols: 1, rows: 1},
-    {text: 'Trainings', cols: 2, rows: 1}
-  ];
+  // tiles: Tile[] = [
+  //   {text: 'Resonare', cols: 3, rows: 1},
+  //   {text: 'Concert Arrangement', cols: 1, rows: 2},
+  //   {text: 'Project Management', cols: 1, rows: 1},
+  //   {text: 'Trainings', cols: 2, rows: 1}
+  // ];
 
   constructor() { }
 
